b4cdbaccafe8949826c4ed798bc9abb38856bbb3
bestkavindu <bestkavindu@gmail.com>
2025-09-17 20:51:16 +0530

    💄 Initialize frontend ui

--- app/Filament/Resources/BrandResource.php
+++ app/Filament/Resources/BrandResource.php
@@ -22,6 +22,8 @@ class BrandResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $recordTitleAttribute = 'name  ';
+
     public static function form(Form $form): Form
     {
         return $form
