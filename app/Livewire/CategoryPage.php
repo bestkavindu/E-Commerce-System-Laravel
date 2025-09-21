48874ed26c6fe0b557fb7f5176852605b408874b
bestkavindu <bestkavindu@gmail.com>
2025-09-21 20:40:33 +0530

    🎨 Category page dynamic to funtionalized

--- app/Livewire/CategoryPage.php
+++ app/Livewire/CategoryPage.php
@@ -2,10 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
 use Livewire\Component;
+use Livewire\Attributes\Title;
+
 
 class CategoryPage extends Component
 {
+    public $categories;
+
+
+    #[Title('Category Page')]
+
+    public function mount()
+    {
+        $this->categories = Category::get();
+    }
     public function render()
     {
         return view('livewire.category-page');
