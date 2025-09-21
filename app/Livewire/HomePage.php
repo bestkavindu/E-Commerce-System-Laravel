71ffc40bdb10046ee8db42041e6e929b2707220c
bestkavindu <bestkavindu@gmail.com>
2025-09-21 20:29:36 +0530

    ✨ Add brands data retrieval to HomePage component

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
 class HomePage extends Component
 {
+    public $brands;
+    #[Title('Home Page')]
+    public function mount()
+    {
+        $this->brands = Brand::get();
+    }
     public function render()
     {
         return view('livewire.home-page');
