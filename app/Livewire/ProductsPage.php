9b14166cb6f5fd011b0d996530761f48acd23c67
bestkavindu <bestkavindu@gmail.com>
2025-09-21 20:51:27 +0530

    🎨  Product data shows

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,10 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\Attributes\Title;
+
 
 class ProductsPage extends Component
 {
+    #[Title('Products Page')]
+
+    public $products;
+
+    public function mount()
+    {
+        $this->products = Product::get();
+    }
     public function render()
     {
         return view('livewire.products-page');
